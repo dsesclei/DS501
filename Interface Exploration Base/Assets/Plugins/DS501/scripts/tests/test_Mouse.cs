30adec01657f34787a83721bb61bcf2ecf1ae751
alex <adpeer@wisc.edu>
2015-11-18 15:43:07 -0600

    cleanup: mouse test, some reformatting

--- Interface Exploration Base/Assets/Plugins/DS501/scripts/tests/test_Mouse.cs
+++ Interface Exploration Base/Assets/Plugins/DS501/scripts/tests/test_Mouse.cs
@@ -3,17 +3,22 @@ using System.Collections;
 
 public class test_Mouse : MonoBehaviour {
 	
-	public GameObject target_group;
+	//public GameObject target_group;
 	
 	// Use this for initialization
 	void Start () {
 
 		Mouse.init ();
 
-		Mouse.onUp_Left  += () => { Debug.Log ("Mouse: Left Up, Velocity: "  + Mouse.velocity); };
-		Mouse.onUp_Right += () => { Debug.Log ("Mouse: right Up, Velocity: " + Mouse.velocity); };
-		Mouse.onDown_Left  += () => { Debug.Log ("Mouse: Left Down, Position: "  + Mouse.position); };
-		Mouse.onDown_Left  += () => { Debug.Log ("Mouse: Left Down, Position: "  + Mouse.position); };
-		Mouse.onMove += () => { Debug.Log ("Mouse move, vel:" + Mouse.velocity); };
+		Mouse.onUp_Left   += 
+		  () => { Debug.Log ("Mouse: Left Up, Velocity: "  + Mouse.velocity); };
+		Mouse.onUp_Right  += 
+		  () => { Debug.Log ("Mouse: right Up, Velocity: " + Mouse.velocity); };
+		Mouse.onDown_Left += 
+		  () => { Debug.Log ("Mouse: Left Down, Position: "  + Mouse.position); };
+		Mouse.onDown_Left += 
+		  () => { Debug.Log ("Mouse: Left Down, Position: "  + Mouse.position); };
+		Mouse.onMove      += 
+		  () => { Debug.Log ("Mouse move, vel:" + Mouse.velocity); };
 	}
 }
