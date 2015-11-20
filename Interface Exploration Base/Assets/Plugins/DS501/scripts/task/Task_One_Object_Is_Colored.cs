694eb33ceed96b23401a0306ea511b8f0c62b48d
Dave Sescleifer <dave@sescleifer.com>
2015-11-20 13:58:58 -0600

    Fix randomness and arrange targets and reset in 9x9 pattern

--- Interface Exploration Base/Assets/Plugins/DS501/scripts/task/Task_One_Object_Is_Colored.cs
+++ Interface Exploration Base/Assets/Plugins/DS501/scripts/task/Task_One_Object_Is_Colored.cs
@@ -1,70 +1,80 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-
 public class Task_One_Object_Is_Colored : Task
 {
 	public bool isOver = false;
-
+	
 	// objects to select from
 	public GameObject targetObject;
 	public GameObject resetObject;
-	public int numTargets = 9;
+	public int numTargets = 8;
 	public int targetsSelected;
-
+	
 	private GameObject[] children;
 	private int[] order;
-
+	private float[] positions = new float[]{
+		-9, 6, 1,
+		0, 6, 1,
+		9, 6, 1,
+		-9, 1.5f, 1,
+		9, 1.5f, 1,
+		-9, -3, 1,
+		0, -3, 1,
+		9, -3, 1
+	};
+	
 	public Task_One_Object_Is_Colored( GameObject targetObject, GameObject resetObject )
 	{
 		this.targetObject = targetObject;
 		this.resetObject = resetObject;
 	}
-
+	
 	// call to start the task
-	public void start ()
+	public void start()
 	{
 		isOver = false;
 		targetsSelected = 0;
-
+		
 		order = new int[numTargets];
 		for (int i = 0; i < numTargets; i++)
 			order[i] = i;
-		new System.Random ().Shuffle (order);
-
-		resetObject.SetActive (true);
-		setColor (resetObject, Color.red);
-
-		targetObject.SetActive (false);
-		setColor (targetObject, Color.red);
+		new System.Random().Shuffle(order);
+		
+		resetObject.SetActive(true);
+		setColor(resetObject, Color.red);
+		
+		targetObject.SetActive(false);
+		setColor(targetObject, Color.red);
 	}
-
-	public void onTargetSelected ()
+	
+	public void onTargetSelected()
 	{
 		targetsSelected++;
 		isOver = targetsSelected >= numTargets;
-		resetObject.SetActive (true);
-		targetObject.SetActive (false);
+		resetObject.SetActive(true);
+		targetObject.SetActive(false);
 	}
-
-	public void onResetSelected ()
+	
+	public void onResetSelected()
 	{
-		Vector3 position = targetObject.gameObject.transform.position;
-		// Would be nice to remove these magic numbers, but probably not worth the time
-		position.x = order[targetsSelected] * 2 - 8;
-		position.y = order[targetsSelected] - 3;
+		int offset = order[targetsSelected] * 3;
+		Vector3 position = new Vector3(
+			positions[offset],
+			positions[offset + 1],
+			positions[offset + 2]
+			);
 		targetObject.gameObject.transform.position = position;
-		resetObject.SetActive (false);
-		targetObject.SetActive (true);
+		resetObject.SetActive(false);
+		targetObject.SetActive(true);
 	}
-
-	public void end ()
+	
+	public void end()
 	{
 		isOver = true;
-		resetObject.SetActive (false);
-		targetObject.SetActive (false);
+		resetObject.SetActive(false);
+		targetObject.SetActive(false);
 	}
-
 	
 	private void setColor( GameObject obj, Color color )
 	{
