8cbbd6d1f3981614136bad221a78b2c8dcee1d6e
alex <na>
2015-11-23 17:27:23 -0600

    software cursor on UI layer for mouse and leap

--- Interface Exploration Base/Assets/DS501/Select_Leap_RightPalm_Screenspace_Cursor.cs
+++ Interface Exploration Base/Assets/DS501/Select_Leap_RightPalm_Screenspace_Cursor.cs
@@ -7,6 +7,7 @@ public class Select_Leap_RightPalm_Screenspace_Cursor
 {
 
     GameObject cursor = GameObject.Find("Software_Cursor");
+    GameObject canvas = GameObject.Find("UI Layer/Canvas");
 
     Action<GameObject> onSelect = null;
 
@@ -29,9 +30,18 @@ public class Select_Leap_RightPalm_Screenspace_Cursor
 
     public void onMove()
     {
-        Vector3 new_pos = Camera.main.WorldToScreenPoint(LeapMotion.pos_right_palm);
-        new_pos.z = 0;
+
+        RectTransform rect = canvas.GetComponent<RectTransform>();
+        RectTransform cursor_rect = cursor.GetComponent<RectTransform>();
+        Vector3 new_pos = Mouse.position;
+        new_pos.z = cursor_rect.position.z;
+        //new_pos.x = (new_pos.x * rect.sizeDelta.x) - (0.5f * rect.sizeDelta.x);
+        //new_pos.y = (new_pos.y * rect.sizeDelta.y) - (0.5f * rect.sizeDelta.y);
+        new_pos.x = (new_pos.x) - (0.5f * rect.sizeDelta.x);
+        new_pos.y = (new_pos.y) - (0.5f * rect.sizeDelta.y);
+
         cursor.transform.position = new_pos;
+        cursor_rect.position = new_pos;
     }
 
     public void onPress()
--- Interface Exploration Base/Assets/Plugins/DS501/scripts/interface/Select_MouseClick_Raycast.cs
+++ Interface Exploration Base/Assets/Plugins/DS501/scripts/interface/Select_MouseClick_Raycast.cs
@@ -5,17 +5,36 @@ using System;
 public class Select_MouseClick_Raycast
 {
 
-	Action<GameObject> onSelect = null;//delegate(GameObject obj) {};
+    GameObject cursor = GameObject.Find("Software_Cursor");
+    GameObject canvas = GameObject.Find("UI Layer/Canvas");
+
+    Action<GameObject> onSelect = null;//delegate(GameObject obj) {};
 
 	public void   register( Action<GameObject> onSelect )	{	this.onSelect += onSelect;	}
 	public void unregister( Action<GameObject> onSelect )	{	this.onSelect -= onSelect;	}
 
 	public Select_MouseClick_Raycast()
 	{
-		// init the input method; register with relevant listeners
-		Mouse.init ();
+        // init the input method; register with relevant listeners
+        Mouse.init ();
 		Mouse.onDown_Left += delegate() { this.onClick(); };
+        Mouse.onMove += this.onMove;
 	}
+
+    public void onMove()
+    {
+        RectTransform rect = canvas.GetComponent<RectTransform>();
+        RectTransform cursor_rect = cursor.GetComponent<RectTransform>();
+        Vector3 new_pos = Mouse.position;
+        new_pos.z = cursor_rect.position.z;
+        //new_pos.x = (new_pos.x * rect.sizeDelta.x) - (0.5f * rect.sizeDelta.x);
+        //new_pos.y = (new_pos.y * rect.sizeDelta.y) - (0.5f * rect.sizeDelta.y);
+        new_pos.x = (new_pos.x) - (0.5f * rect.sizeDelta.x);
+        new_pos.y = (new_pos.y) - (0.5f * rect.sizeDelta.y);
+        Debug.Log("New pos: " + new_pos);
+        //Debug.Log("RectTransform: " + rect.sizeDelta);
+        cursor_rect.position = new_pos;
+    }
 
 	public void onClick()
 	{
