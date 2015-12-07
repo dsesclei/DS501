50009461b49bc811b527a101f5c934af0ae22ed8
alex <adpeer@wisc.edu>
2015-12-07 16:28:37 -0600

    added: Desk

---
+++ Desk/Assets/DS501/core/JSON.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+//using MiniJSON;
+
+public class JSON
+{
+	public static Dictionary<String,System.Object> read( string filename )
+	{
+		StreamReader in_file = File.OpenText( filename );
+		var data_JSON = in_file.ReadToEnd( );
+		in_file.Close();
+		
+		return MiniJSON.Json.Deserialize( data_JSON ) as Dictionary<String,System.Object>;
+	}
+}
