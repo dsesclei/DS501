f2b1f61793519f8479667589a933c4d65d7c3d57
Ryan Koesters <rmkoesters@gmail.com>
2015-12-13 19:42:50 -0600

    Start sort_by_color and make objects interact.

--- Desk/Assets/DS501/Run_test_Ryan.cs
+++ Desk/Assets/DS501/Run_test_Ryan.cs
@@ -6,6 +6,6 @@ public class Run_test_Ryan : Run_Desk {
     
     public override void MakeMinigames()
     {
-        AddMinigame( new select_three() );
+        AddMinigame( new sort_by_color() );
     }
 }
---
+++ Desk/Assets/DS501/minigames/sort_by_color.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class sort_by_color : Minigame
+{
+    List<GameObject> objects = new List<GameObject>();
+    Plane plane;
+
+    int total_objects = 6;
+    float z_distance = 0.4f;
+
+    public override void init()
+    {
+        name = "sort_by_color";
+        instructions = "Sort the objects by color.";
+
+        helper.showCursor();
+
+        plane = helper.getGamePlane(depth: z_distance);
+
+        for (int i = 0; i < total_objects; i++)
+        {
+            GameObject obj = helper.getAnObject();
+            if (i % 2 == 0)
+            {
+                setColor(obj, Color.blue);
+                obj.name = "blue";
+            }
+            else
+            {
+                setColor(obj, Color.red);
+                obj.name = "red";
+            }
+            objects.Add(obj);
+        }
+
+        float max_x = Camera.main.pixelWidth,
+              max_y = Camera.main.pixelHeight;
+        foreach (GameObject obj in objects)
+        {
+            Vector3 random_screenpos = new Vector3(Random.Range(0, max_x), Random.Range(0, max_y), z_distance);
+
+            obj.transform.position = Camera.main.ScreenToWorldPoint(random_screenpos);
+        }
+        
+        helper.onDrag += (GameObject selected) =>
+        {
+            Debug.Log("OnDrag");
+            Vector3 world_pos = selected.transform.position;
+            Vector3 screen_pos = Camera.main.WorldToScreenPoint(world_pos);
+            screen_pos.x += helper.screenspace_velocity.x;
+            screen_pos.y += helper.screenspace_velocity.y;
+            selected.transform.position = misc.ScreenspacePointToPlane(plane, screen_pos);
+        };
+    }
+    
+    public override void update()
+    {
+
+    }
+
+    private void setColor(GameObject obj, Color color)
+    {
+        foreach (Renderer r in obj.GetComponents<Renderer>())
+        {
+            foreach (Material m in r.materials)
+            {
+                m.color = color;
+            }
+        }
+    }
+}
